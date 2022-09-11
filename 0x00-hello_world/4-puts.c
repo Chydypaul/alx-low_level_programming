5440b0efcb3cb47486daa1e6e4ac862e22e00352
Chidi Obah <paulobah34@gmail.com>
2022-09-11 10:47:46 -0700

    Hello Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - print the string in the put function
  * Description: using the main function
- * this program prints "programming is like buildiing a multilingual puzzle"
+ * this program prints "Programming is like buildiing a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
