734aa50601afc7a9eae0d504a5f23c2e0e2570a0
Chidi Obah <paulobah34@gmail.com>
2022-09-12 18:59:40 -0700

    Print 102.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,10 +14,10 @@ int main(void)
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
-			putchar((num % 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(32);
-			putchar((num / 10) + '0');
-			putchar((num % 10) + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 			if (num1 / 10 != 9 || num1 % 10 != 8)
 			{
 				putchar(44);
