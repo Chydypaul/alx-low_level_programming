be72528edcca39f1d177e685f3e43ef23c58fd9d
Chidi Obah <paulobah34@gmail.com>
2022-09-28 07:24:43 -0700

    5 strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,28 +8,22 @@
  */
 
 char *_strstr(char *haystack, char *needle)
-int position(char *s1, char *s2) 
 {
 	int i, j;
 
-	for (i = 0; haystack[i]; i++) 
+	for (i = 0; haystack[i] > '\0'; j++)
 	{
-		for (j = 0; needle[j] && needle[j] == haystack[i + j]; j++);
-		if (!needle[j]) return i;
+		for (i = 0; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+		{
+			if (haystack[j] != needle[j - i])
+			{
+				break;
+			}
+		}
+		if (needle[j - i] == '\0')
+		{
+			return (haystack + i);
+		}
 	}
-	return -1;
-}
-
-int main() 
-{
-	char word1[101], word2[101];
-	int p;
-	printf("Type two words: ");
-	scanf("%s %s", word1, word2);
-	p = position(word1, word2);
-	if (p < 0) 
-		printf("Word'%s' does not exists in the sentence '%s'.", word2, word1);
-	else
-		printf("Position of the word '%s' is %d.", word2, p);
-	return 0;
+	return (0);
 }
